621d0ab8df3993c8f585b319f98d2a5640f4748e
MhassanS23 <maulanahassan366@gmail.com>
2023-05-22 15:17:19 +0700

    done

--- src/firebase.js
+++ src/firebase.js
@@ -15,13 +15,13 @@ import {
 } from 'firebase/firestore';
   
 const firebaseConfig = {
-    apiKey: 'AIzaSyA2JVBWjrGV-vWFgR7GJknEbTvSeO5V9Xo',
-    authDomain: 'autentication-b5e9a.firebaseapp.com',
-    projectId: 'autentication-b5e9a',
-    storageBucket: 'autentication-b5e9a.appspot.com',
-    messagingSenderId: '604542871023',
-    appId: '1:604542871023:web:420f97ac62904265fa421a',
-    measurementId: 'G-Y7EZKP230K'
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESAGGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
